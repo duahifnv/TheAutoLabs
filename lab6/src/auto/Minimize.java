58846610226d59dd42cdb4cf9f6f00ad5bc9eea4
duahifnv <max135603@gmail.com>
2024-04-08 22:23:27 +0300

    Исправлено исключение ConcurrentModificationException

--- lab6/src/auto/Minimize.java
+++ lab6/src/auto/Minimize.java
@@ -26,44 +26,48 @@ public class Minimize {
             }
             else classes.get(1).add(auto);;
         }
-        // Проход по неразбитым классам пока они есть
         List<List<Auto>> finalClasses = new ArrayList<>();
-        for (List<Auto> clazz : List.copyOf(classes)) {
-            // Класс состоит из одного элемента
-            if (clazz.size() == 1) {
-                finalClasses.add(clazz);
-                classes.remove(clazz);
-                continue;
-            }
-            boolean gotSlice = false;
-            for (int i = 0; i < alph.size() - 1; i++) {
-                for (Auto auto : clazz) {
-                    Auto jump = automata.get(auto).get(i);  // p(i) -> p(j)
-                    // Если нет перехода
-                    if (jump == null) continue;
-                    // Класс содержит переходы не из своего класса
-                    if (!clazz.contains(jump)) {
-                        clazz.remove(auto); // Удаляем элемент из класса
-                        // Добавляем новый класс из одного элемента в финальный список
-                        finalClasses.add(new ArrayList<>(Collections.singleton(auto)));
-                        gotSlice = true;   // Разбили класс хотя бы раз
-                        break;
-                    }
+        // Проход по неразбитым классам пока они есть
+        while (classes.size() != 0) {
+            for (List<Auto> clazz : List.copyOf(classes)) {
+                // Класс состоит из одного элемента
+                if (clazz.size() == 1) {
+                    finalClasses.add(clazz);
+                    classes.remove(clazz);
+                    if (classes.size() == 0) break;
+                    continue;
                 }
-                if (!gotSlice) continue;
-                // Остановка разбиения класса
-                break;
-            }
-            if (!gotSlice) {
-                classes.remove(clazz); // Удаляем класс из неразбитых классов
-                finalClasses.add(clazz); // Добавляем класс в финальный список
+                boolean gotSlice = false;
+                for (int i = 0; i < alph.size() - 1; i++) {
+                    for (Auto auto : clazz) {
+                        Auto jump = automata.get(auto).get(i);  // p(i) -> p(j)
+                        // Если нет перехода
+                        if (jump == null) continue;
+                        // Класс содержит переходы не из своего класса
+                        if (!clazz.contains(jump)) {
+                            clazz.remove(auto); // Удаляем элемент из класса
+                            // Добавляем новый класс из одного элемента в финальный список
+                            finalClasses.add(new ArrayList<>(Collections.singleton(auto)));
+                            gotSlice = true;   // Разбили класс хотя бы раз
+                            break;
+                        }
+                    }
+                    if (!gotSlice) continue;
+                    // Остановка разбиения класса
+                    break;
+                }
+                if (!gotSlice) {
+                    classes.remove(clazz);; // Удаляем класс из неразбитых классов
+                    if (classes.size() == 0) break;
+                    finalClasses.add(clazz); // Добавляем класс в финальный список
+                }
             }
         }
         System.out.println("Классы эквивалентности:");
         for(List<Auto> clazz : finalClasses) {
             List<String> values = clazz.stream().map(x -> "q" + x.getIdx().toString()).toList();
             String format = String.join(",", values);
-            System.out.println("{ " + format + " }");
+            System.out.println("[" + format + "]");
         }
     }
 }
