2d6783639ba20a88b36d730700925cf75b521ce1
duahifnv <max135603@gmail.com>
2024-04-08 14:08:18 +0300

    Лабораторная работа #6 "Минимизация конечных автоматов-распознавателей"

---
+++ lab6/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Лабораторная работа #6
+ * "Минимизация конечных автоматов-распознавателей"
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Лаба 6");
+    }
+}
