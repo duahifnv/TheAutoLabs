3cc93f33bdec1b496e90bac8149f06813e2db2ba
duahifnv <max135603@gmail.com>
2024-04-11 15:46:02 +0300

    Добавлен ввод с консоли простейших элементов

---
+++ 6. Minimize/src/auto/Input.java
@@ -0,0 +1,68 @@
+package auto;
+
+import java.util.*;
+
+/**
+ * Ввод с консоли
+ */
+public class Input {
+    private final String exitWord;
+    protected Scanner scan = new Scanner(System.in);
+    public Input(String exitWord) {
+        this.exitWord = exitWord;
+    }
+    public int Size(String comm, int min, int max) {
+        int input;
+        while(true) {
+            System.out.print(comm);
+            try {
+                input = scan.nextInt();
+            } catch (Exception NumberFormatException) {
+                System.out.println("Введенный символ не является числом");
+                scan.nextLine();
+                continue;
+            }
+            if (input < min || input > max) {
+                System.out.println("Введенное число вне диапазона");
+                continue;
+            }
+            break;
+        }
+        return input;
+    }
+    public Set<Integer> Set(String main_comm, String comm, int br, int min, int max) {
+        System.out.print(main_comm);
+        System.out.printf(" (%d, чтобы закончить)%n", br);
+        Set<Integer> set = new HashSet<>();
+        while (true) {
+            int num = Size(comm + ": ", -1, max);
+            if(num == br) {
+                if (set.size() < min) {
+                    System.out.printf("Введите хотя бы %d чисел%n", min);
+                    continue;
+                }
+                break;
+            }
+            set.add(num);
+        }
+        return set;
+    }
+    public String Stroke(String comm, List<Character> alph) {
+        String stroke;
+        while(true) {
+            System.out.print(comm + ": ");
+            stroke = scan.nextLine();
+            if (stroke.equals(exitWord)) break;
+            boolean isValid = true;
+            for (Character ch : stroke.toCharArray()) {
+                if (!alph.contains(ch)) {
+                    System.out.println("В введенном слове есть символы не из алфавита");
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) break;
+        }
+        return stroke;
+    }
+}
