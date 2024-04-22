aa74d24b1f16dd61dbcf0acabac159cec2c66b94
duahifnv <max135603@gmail.com>
2024-04-22 14:38:08 +0300

    Логика

--- Grammar7/src/Main.java
+++ Grammar7/src/Main.java
@@ -1,5 +1,11 @@
+import grammar.Grammar;
+import json.JSONmanager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String jsonPath = "D:\\DSTU\\автоматы\\TheAutoLabs\\Grammar7\\json\\grammar8.json";
+        Grammar grammar = JSONmanager.parseObject(jsonPath, Grammar.class);
+        System.out.println(grammar.getRules());
+        
     }
 }
