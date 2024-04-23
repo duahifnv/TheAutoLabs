16fba79fd867a98928592cf452daaf5025430829
duahifnv <max135603@gmail.com>
2024-04-23 13:18:06 +0300

    Рефакторинг

--- Grammar7/src/Main.java
+++ Grammar7/src/Main.java
@@ -1,11 +1,11 @@
 import grammar.Grammar;
+import grammar.Type;
 import json.JSONmanager;
 
 public class Main {
     public static void main(String[] args) {
         String jsonPath = "D:\\DSTU\\автоматы\\TheAutoLabs\\Grammar7\\json\\grammar8.json";
         Grammar grammar = JSONmanager.parseObject(jsonPath, Grammar.class);
-        System.out.println(grammar.getRules());
-        
+        System.out.println(grammar);
     }
 }
