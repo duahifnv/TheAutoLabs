06144f57d7679d3803128ec37bfd5f02e877a964
duahifnv <max135603@gmail.com>
2024-04-23 13:17:54 +0300

    Рефакторинг

--- Grammar7/src/grammar/Grammar.java
+++ Grammar7/src/grammar/Grammar.java
@@ -1,18 +1,33 @@
 package grammar;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public record Grammar(String terminal,
                       String non_terminal,
-                      String rules) {
+                      String rules,
+                      String start) {
     public List<Rule> getRules() {
         List<Rule> ruleList = new ArrayList<>();
         for (String rule : rules.split("&")) {
             String[] sides = rule.split("->");
-            ruleList.add(new Rule(sides[0], Arrays.stream(sides[1].split("\\|")).toList()));
+            for (String right : sides[1].split("\\|")) {
+                ruleList.add(new Rule(sides[0], right));
+            }
         }
         return ruleList;
+    }
+    public Type getType() {
+        TypeDetector typeDetector = new TypeDetector(this);
+        return (typeDetector.getType());
+    }
+    @Override
+    public String toString() {
+        return "Грамматика {" +
+                "Терминалы: [" + terminal + ']' +
+                ", Нетерминалы [" + non_terminal + ']' +
+                ", Правила " + getRules() +
+                ", Стартовый символ: " + start +
+                ", \nКлассификация по Хомскому: " + this.getType().getLabel() + '}';
     }
 }
