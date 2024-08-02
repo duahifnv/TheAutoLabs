76fd375a55921a9b7aecc21447082f0635f8ccb3
duahifnv <max135603@gmail.com>
2024-08-02 16:08:51 +0300

    .

--- Grammar7/src/Main.java
+++ Grammar7/src/Main.java
@@ -1,9 +1,10 @@
 import grammar.Grammar;
+import grammar.Type;
 import json.JSONmanager;
 
 public class Main {
     public static void main(String[] args) {
-        String jsonPath = "D:\\DSTU\\автоматы\\TheAutoLabs\\Grammar7\\json\\grammar8.json";
+        String jsonPath = "D:\\DSTU\\автоматы\\TheAutoLabs\\Grammar7\\json\\task3.json";
         Grammar grammar = JSONmanager.parseObject(jsonPath, Grammar.class);
         System.out.println(grammar);
     }
